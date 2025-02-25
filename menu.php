e4951faf939e50162105c9d7ebdaca801e073e7f
axssatt <anniesaruta47@gmail.com>
2025-02-25 19:51:38 +0700

    add menu complete

--- menu.php
+++ menu.php
@@ -16,12 +16,15 @@
     <?php include_once "navbar.php"; ?>
     <div class="container-fluid">
         <div class="row my-3">
-            <div class="col col-md-1 col-lg-2">
+            <div class="col col-md-1 col-lg-1">
                 <!-- ห้ามเขียนโค้ดตรงนี้ -->
             </div>
-            <div class="col-12 col-md-10 col-lg-8">
+            <div class="col-12 col-md-10 col-lg-10">
                 <div class="row">
                     <p class="text-center fs-4">Menu</p>
+                </div>
+                <div>
+                    <p class="text-start fs-6">Coffee</p>
                 </div>
                 <div class="row my-2">
                     <!-- ไปทำต่อเองเริ่มให้แล้ว -->
@@ -30,12 +33,59 @@
 
                     while ($menu = mysqli_fetch_assoc($result)) {
                 ?>
-                    <div class="col-12 col-md-3 col-lg-3">
+                    <div class="col-12 col-md-3 ol-lg-3">
                         <div class="card">
                             <img src="img-upload/<?= $menu['img']; ?>" class="card-img-top">
                             <div class="card-body">
                                 <h4 class="card-title"><?= $menu['name']; ?></h4>
                                 <p style="font-size: 12px;"><?= $menu['description']; ?></p>
+                                <p style="font-size: 25px;"><?= $menu['price']; ?>฿</p>
+                                <a href="orderMenuCoffee.php?id=<?= $menu['menu_id']; ?>" class="btn btn-primary">Order</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+                </div>
+                <div>
+                    <p class="text-start fs-6">Non Coffee</p>
+                </div>
+                <div class="row my-2">
+                    <!-- ไปทำต่อเองเริ่มให้แล้ว -->
+                <?php $queryMenuCoffee = "SELECT * FROM menu WHERE Coffee = 'no'"; 
+                    $result = mysqli_query($connect, $queryMenuCoffee);
+
+                    while ($menu = mysqli_fetch_assoc($result)) {
+                ?>
+                    <div class="col-12 col-md-3 ol-lg-3">
+                        <div class="card">
+                            <img src="img-upload/<?= $menu['img']; ?>" class="card-img-top">
+                            <div class="card-body">
+                                <h4 class="card-title"><?= $menu['name']; ?></h4>
+                                <p style="font-size: 12px;"><?= $menu['description']; ?></p>
+                                <p style="font-size: 25px;"><?= $menu['price']; ?>฿</p>
+                                <a href="orderMenuCoffee.php?id=<?= $menu['menu_id']; ?>" class="btn btn-primary">Order</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+                </div>
+                <div>
+                    <p class="text-start fs-6">Dessert</p>
+                </div>
+                <div class="row my-2">
+                    <!-- ไปทำต่อเองเริ่มให้แล้ว -->
+                <?php $queryMenuCoffee = "SELECT * FROM menu WHERE Coffee = 'des'"; 
+                    $result = mysqli_query($connect, $queryMenuCoffee);
+
+                    while ($menu = mysqli_fetch_assoc($result)) {
+                ?>
+                    <div class="col-12 col-md-3 ol-lg-3">
+                        <div class="card">
+                            <img src="img-upload/<?= $menu['img']; ?>" class="card-img-top">
+                            <div class="card-body">
+                                <h4 class="card-title"><?= $menu['name']; ?></h4>
+                                <p style="font-size: 12px;"><?= $menu['description']; ?></p>
+                                <p style="font-size: 25px;"><?= $menu['price']; ?>฿</p>
                                 <a href="orderMenuCoffee.php?id=<?= $menu['menu_id']; ?>" class="btn btn-primary">Order</a>
                             </div>
                         </div>
@@ -43,7 +93,8 @@
                 <?php } ?>
                 </div>
             </div>
-            <div class="col col-md-1 col-lg-2">
+            </div>
+            <div class="col col-md-1 col-lg-1">
                 <!-- ห้ามเขียนโค้ดตรงนี้ -->
             </div>
         </div>
