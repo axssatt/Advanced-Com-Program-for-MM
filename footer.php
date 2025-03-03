66fcdfe02ff3c618b34203d3143f459fc75d6e13
korakrit-c <korakrit_c@cmu.ac.th>
2025-03-03 22:28:40 +0700

    fixed footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div style="background-color: #6F8C51; color: #fff;" class="p-2" position: fixed;>
+<div style="background-color: #6F8C51; color: #fff; margin-top: auto;" class="p-2" >
     <p style="text-align: center; font-size: 12px;" class="mx-5">Copyright ©2025 Nattharawipa, Saruta and Punmanus. This project was developed as part of the Advanced Computer Programming for Modern Management course.<br>
     Unauthorized reproduction, distribution, or modification is prohibited without prior permission</p>
 </div>
