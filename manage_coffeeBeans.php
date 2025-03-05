4db69d40f72d5ef4cae86b3d52c583c45bf3b587
axssatt <anniesaruta47@gmail.com>
2025-03-05 16:34:58 +0700

    Update manage_coffeeBeans.php

--- manage_coffeeBeans.php
+++ manage_coffeeBeans.php
@@ -56,7 +56,48 @@ if(isset($_GET['del'])) {
                             ?>
                                 <tr>
                                     <td><?= $menu['name']; ?></td>
-                                    <td><a href="updateMenu.php?id=<?= $menu['goods_id']; ?>" class="btn btn-warning w-100">Edit</a></td>
+                                    <td>
+                                        <button type="button" class="btn btn-warning w-100" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $menu['goods_id']; ?>">
+                                            Edit
+                                        </button>
+
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal<?= $menu['goods_id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel<?= $menu['goods_id']; ?>" aria-hidden="true">
+                                            <div class="modal-dialog modal-dialog-centered">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h1 class="modal-title fs-5" id="exampleModalLabel<?= $user['user_id']; ?>">Edit Coffeebeans</h1>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <form action="manage_coffeeBeans.php" method="post">
+                                                        <div class="modal-body">
+                                                            <input type="hidden" name="userId" value="<?= $user['user_id']; ?>">
+                                                            <div class="mb-2">
+                                                                <label for="name" class="form-label">Cofeebeans Name</label>
+                                                                <input type="text" name="name" id="name" class="form-control" value="<?= $menu['name']; ?>" required>
+                                                            </div>
+                                                            <div class="mb-2">
+                                                                <label for="descriton" class="form-label">Description</label>
+                                                                <input type="text" name="description" id="description" class="form-control" value="<?= $menu['description']; ?>" required>
+                                                            </div>
+                                                            <div class="mb-2">
+                                                                <label for="price" class="form-label">Price</label>
+                                                                <input type="text" name="price" id="price" class="form-control" value="<?= $menu['price']; ?>" required>
+                                                            </div>
+                                                            <div class="mb-2">
+                                                                <label for="quantity" class="form-label">Quantity</label>
+                                                                <input type="text" name="password" id="password" class="form-control" value="<?= $menu['quantity']; ?>" required>
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                            <button type="submit" name="updateUser" class="btn btn-warning">Save changes</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
                                     <td><a data-id="<?= $menu['goods_id'];?>" href="manage_coffeeBeans.php?del=<?= $menu['goods_id']; ?>" class="btn btn-danger w-100 delete-btn">Delete</a></td>
                                 </tr>
                             <?php } } ?>
